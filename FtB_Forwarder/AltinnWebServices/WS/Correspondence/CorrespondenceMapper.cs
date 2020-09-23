95f90a6a5dcb3d77c279053db560e8d18c107036
Øystein Thoen Sisjord <oystein@arkitektum.no>
2020-09-23 12:25:57 +0200

    Added correspondence

---
+++ FtB_Forwarder/AltinnWebServices/WS/Correspondence/CorrespondenceMapper.cs
@@ -0,0 +1,22 @@
+﻿using AltinnWebServices.WS.Correspondence.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AltinnWebServices.WS.Correspondence
+{
+    public class CorrespondenceMapper
+    {
+        public static CorrespondenceItem Map(InsertCorrespondenceV2 insertCorrespondenceV2)
+        {
+
+            return null;
+        }
+
+        public static InsertCorrespondenceV2 Map(CorrespondenceItem correspondenceItem)
+        {
+
+            return null;
+        }
+    }
+}
